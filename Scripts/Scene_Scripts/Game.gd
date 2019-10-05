201679f526ad500f89025a8d63bd5a6eca82ffc1
Bram <bramreth@gmail.com>
2019-10-05 13:01:33 +0100

    added builder mode shader

--- Scripts/Scene_Scripts/Game.gd
+++ Scripts/Scene_Scripts/Game.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 onready var navigation: Navigation2D = $Map/Navigation
 
+var building = true
 var selectedCharacter = null
 var selectedEntity = null
 
@@ -29,3 +30,12 @@ func move_character(target):
 	if selectedCharacter != null:
 		var path = navigation.get_simple_path(selectedCharacter.position, target, false)
 		selectedCharacter.move(path)
+		
+		
+#handle build tool
+func _physics_process(delta):
+	if building:
+		var mouse_pos = get_global_mouse_position()
+		
+		var tile = $Map/Navigation/Map.world_to_map(mouse_pos)
+		$Map/Navigation/YSort/build_tool.position =  $Map/Navigation/Map.map_to_world(tile) +Vector2(0, $Map/Navigation/Map.cell_size.y/2)
