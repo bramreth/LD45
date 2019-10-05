684097515914d2a3f8fc5ab7a66528f092a346b9
Bram <bramreth@gmail.com>
2019-10-05 01:38:21 +0100

    tutorial

---
+++ Scenes/tutorial.gd
@@ -0,0 +1,13 @@
+extends Control
+
+var text_queue = []
+
+# this is the tutorial script, we want to write text in a specifiv order
+func _ready():
+	pass # Replace with function body.
+
+func confirm_dialog():
+	pass
+
+func _on_Button_pressed():
+	confirm_dialog()
