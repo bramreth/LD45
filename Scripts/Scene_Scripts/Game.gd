8e76d06129993de5883c5a1cde4ad3720c3764f8
Bram <bramreth@gmail.com>
2019-10-07 22:42:05 +0100

    lots of fixes

--- Scripts/Scene_Scripts/Game.gd
+++ Scripts/Scene_Scripts/Game.gd
@@ -20,6 +20,7 @@ onready var player = $Map/Navigation/YSort/Player
 var mousePos = Vector2()
 
 func _ready():
+	GameManager.start_of_daytime_tick()
 	GameManager.connect("spawn_items", self, "spawn_items")
 	GameManager.connect("spawn_goblin", self, "spawn_goblin")
 	GameManager.connect("night_started", self, "start_night")
